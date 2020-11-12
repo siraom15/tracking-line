864cfeec7371f45bb2af7a9d81c7baba652b47c9
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-11-13 01:09:01 +0700

    [ Add ] Reply

---
+++ reply.js
@@ -0,0 +1,22 @@
+function reply(reply_token, msg, line_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {' + line_token + '}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: msg
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+
+module.exports = reply;
