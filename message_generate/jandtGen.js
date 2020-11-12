42aa80169b20e6e4c555bcf85cdadce300d09eab
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-11-13 01:52:50 +0700

    //

--- message_generate/jandtGen.js
+++ message_generate/jandtGen.js
@@ -4,18 +4,23 @@ const getStatus = require('../api/jandt');
 async function generate(billcode) {
     return await getStatus(billcode).then((data) => {
         let details = data.details[0];
-        var str = "";
-        if (details.length > 0) {
+        let str = "";
+        try {
+
             str = `สถานะ -> J&T Express เลข ${data.billcode} \n`;
             str += `สถานะ : ` + data.status + '\n'
             str += `สถานที่ : ${details.state} ${details.area} ${details.city} \n`
             str += `เวลา : ${details.accepttime}`
+            return str;
+        } catch (err) {
+            console.log(err);
         }
-        else {
-            str += "ไม่พบหมายเลขนี้"
-        }
-        return str;
     });
 }
 
+// async function test() {
+//     let x = await getStatus("623339712601");
+//     console.log(x);
+// }
+// test()
 module.exports = generate;
